409046b8a3a47bdee96abb25b0558237fe1ab422
sunung007@hanyang.ac.kr <KellanLukas1103!>
2021-02-03 16:58:36 +0900

    icon change in scriptable application.

--- setting.js
+++ setting.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: cog;
+// icon-color: orange; icon-glyph: calendar-alt;
 const localFM = FileManager.local()
 const iCloud = FileManager.iCloud()
 
